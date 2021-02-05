8b293a84fed62da8b80d8f7535228a4a6d0bca9f
ashpex <ashpex@pm.me>
2021-02-06 04:04:52 +0700

    reformatting code

--- app/src/main/java/com/ashpex/diceroller/MainActivity.kt
+++ app/src/main/java/com/ashpex/diceroller/MainActivity.kt
@@ -6,6 +6,10 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,23 +19,26 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             rollDice()
-            //val resultTextView: TextView = findViewById(R.id.textView)
-            //resultTextView.text = "6"
-            //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            //toast.show()
+            // Display a toast indicating that the dice has been rolled
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
         }
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
     }
 }
 
-class Dice(private val numSides: Int){
-    fun roll() : Int{
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
